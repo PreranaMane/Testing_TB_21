3eed81898da9e4c6dc7c18f523b29ba3f78626e6
TapeshwarSherekar <149191135+TapeshwarSherekar@users.noreply.github.com>
2023-11-05 21:33:40 +0530

    by jayashree git

--- Git_Program/src/com/Demo/Demo_Program.java
+++ Git_Program/src/com/Demo/Demo_Program.java
@@ -3,11 +3,7 @@ package com.Demo;
 public class Demo_Program 
 {
 	
-	public void m3()
-	{
-		System.out.println("M3-Method");
-	}
-
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Prerna Mame");
